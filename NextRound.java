0ce152ab944a557b1fa2cc50aa8b0ec63117a1e6
laxmanp090404 <laxman.p2022cse@sece.ac.in>
2024-04-05 21:46:51 +0530

    Solved NextRound

---
+++ NextRound.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NextRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        int count = 0;
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>= arr[k-1] && arr[i]>0){
+                  count++; 
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
